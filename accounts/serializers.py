648bf342d60dcdf236e3ab8de621701413b66168
djumanov <djumanovdev@gmail.com>
2025-12-03 18:09:39 +0500

    login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -34,3 +34,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         return user
     
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128)
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import authenticate
+
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
 
 
 class RegisterView(APIView):
@@ -18,3 +21,21 @@ class RegisterView(APIView):
             return Response(user_json, status=status.HTTP_201_CREATED)
         
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            data = serializer.validated_data
+            user = authenticate(username=data['username'], password=data['password'])
+
+            if user is not None:
+                token = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key}, status=status.HTTP_200_OK)
+            
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
